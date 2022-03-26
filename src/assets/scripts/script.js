980b87ea28a24a825170ca9be2472be05c34bd3b
DmitriyR54 <45ryirtimd@gmail.com>
2022-03-26 17:40:34 +0200

    about us done

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -2,9 +2,36 @@
 
 const nav = document.querySelector('.header__nav');
 const navBtn = document.querySelector('.header__nav-btn');
-navBtn.addEventListener('click', toggleNav);
+navBtn.onclick = toggleNav;
 
-function toggleNav(event){
+function toggleNav() {
     nav.classList.toggle('show-nav');
     navBtn.classList.toggle('is-active');
 }
+
+// ABOUT US
+
+let flashlightActive = true;
+
+const aboutUsText = document.querySelector('.aboutus__wrapper');
+const flashlight = document.querySelector('.aboutus__flashlight');
+const flashlightBtn = flashlight.querySelector('.aboutus__flashlight-btn');
+
+function toggleFlashlight(event) {
+    const highlightCondition = event.target.className === 'aboutus__flashlight-img' || event.target.className === 'aboutus__flashlight-btn';
+
+    if (flashlightActive && highlightCondition) {
+        flashlightActive = false;
+        flashlightBtn.classList.toggle('aboutus__flashlight-btn-active');
+        aboutUsText.classList.toggle('aboutus-highlighted');
+        setTimeout(() => flashlight.classList.toggle('flashlight-active'), 100);
+    }
+    else if (!flashlightActive && highlightCondition) {
+        flashlightActive = true;
+        flashlightBtn.classList.toggle('aboutus__flashlight-btn-active');
+        flashlight.classList.toggle('flashlight-active');
+        setTimeout(() => aboutUsText.classList.toggle('aboutus-highlighted'), 100);
+    }
+}
+
+flashlight.onclick = toggleFlashlight;
